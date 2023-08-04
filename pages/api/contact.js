c69f28132e96ab2f4a24741b7aff2e7d0527beff
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-08-04 17:53:55 +0100

    Create api for sending message

---
+++ pages/api/contact.js
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    const newMessage = {
+      name,
+      email,
+      message,
+    };
+
+    let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.umkbcep.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
+    try {
+      client = await MongoClient.connect(connectionString);
+    } catch (error) {
+      res.status(500).json({ message: "Connection to database failed" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message", message: newMessage });
+  }
+}
+
+export default handler;
