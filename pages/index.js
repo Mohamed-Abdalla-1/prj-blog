0c21da91543f677ce36e8c36d1d36e6e218de565
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-08-04 17:53:24 +0100

    Create main app page

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,15 @@ import { Fragment } from "react";
 import Hero from "../component/home-page/hero";
 import FeaturedPosts from "../component/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
-  // const dummy = [
-  //   {
-  //     title: "1T",
-  //     image: "getting-started-nextjs.png",
-  //     excerpt: "1eeeeeeeeeeeeeeeeeeeee",
-  //     date: "2022-10-4",
-  //     slug: "s copy 2",
-  //   },
-  //   {
-  //     title: "2T",
-  //     image: "getting-started-nextjs.png",
-  //     excerpt: "2022-10-4",
-  //     date: "2eeeeeeeeeeeeeeeee",
-  //     slug: "s copy",
-  //   },
-  //   {
-  //     title: "3T",
-  //     image: "getting-started-nextjs.png",
-  //     excerpt: "3eeeeeeeeeeeeeeeeeeee",
-  //     date: "2022-10-4",
-  //     slug: "s",
-  //   },
-  // ];
-
   return (
     <Fragment>
+      <Head>
+        <title>Next blog</title>
+        <meta name="description" content="programming" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
