2812e5f7e11c8bcbcafeddff6d4ae3a0aa62acc2
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-08-04 01:04:23 +0100

    Create contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
+import { Fragment } from "react";
 import ContactForm from "../component/contact/contact-form";
+import Head from "next/head";
 
 function ContactPage() {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact me</title>
+        <meta name="description" content="send messages!" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
 
 export default ContactPage;
